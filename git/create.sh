9f867ddc38af24e3ae294a4ac6a2018928535f55
Shekinah-Ineza <m.ineza1@alustudent.com>
2025-03-19 13:37:07 -0700

    all

---
+++ git/create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Switch to main branch
+
+# Pull latest changes from remote
+echo "git pull origin main" > up_to_date
+
+# Push merged changes to remote
+
+echo "*~" > .gitignore
+
+
+
