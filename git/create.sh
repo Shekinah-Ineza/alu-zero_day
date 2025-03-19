2557dd2ea1891c9e602ccadea96aa9c10ea32624
Shekinah-Ineza <m.ineza1@alustudent.com>
2025-03-19 13:15:52 -0700

    My personal work

---
+++ git/create.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create empty file
+touch bash/98
+
+# Update bash/best
+echo "#!/bin/bash" > bash/best
+echo "echo \"Best School\"" >> bash/best
+
+# Update bash/school
+echo "#!/bin/bash" > bash/school
+echo "echo \"The school is open!\"" >> bash/school
